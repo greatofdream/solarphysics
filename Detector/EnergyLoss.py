e193b28e4a915db6c7e9ebab95503cd98f9d34c7
greatofdream <greatofdream@gmail.com>
2024-11-21 15:52:18 +0800

    adjust the mean excitation energy value; use kinetic energy

--- Detector/EnergyLoss.py
+++ Detector/EnergyLoss.py
@@ -3,29 +3,31 @@ import argparse
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 from matplotlib.ticker import MultipleLocator
-I = 75*1e-6 # MeV
+plt.style.use("../journal.mplstyle")
+I = 73 * 1e-6 # MeV，平均电离和激发能
 N_H, Z_H, A_H, n_H = 1, 1, 1, 2
 N_O, Z_O, A_O, n_O = 1, 8, 16, 1
-N_A = 6.02*1e23
-e = 1.6*1e-19
-J2MeV = 6.24*1e12
-epislon_0 = 8.854*1e-12
+N_A = 6.02*1e23 # 
+e = 1.6*1e-19 # charge, Coulomb
+J2MeV = 6.24*1e12 # Jour to MeV
+epislon_0 = 8.854*1e-12 # permittivity
 m_0 = 0.511
 rho = 1000 #kg/m^3
-N = N_A*rho*1000/(A_H*n_H+A_O*n_O)
+N = N_A*rho*1000/(A_H*n_H+A_O*n_O) # number of H2O in 1m^3
 Z = Z_H + Z_O
-N_H, N_O = N*n_H, N*n_O
-omegas = np.array([A_H*n_H,A_O*n_O])/(A_H*n_H+A_O*n_O)
+N_H, N_O = N*n_H, N*n_O # number of H, O in 1m^3
+omegas = np.array([A_H*n_H,A_O*n_O])/(A_H*n_H+A_O*n_O)# mass percent
 Zs = np.array([Z_H, Z_O])
 Ns = np.array([N_H, N_O])
-n = 1.33
+n = 1.33 # refraction index of water
 if __name__=="__main__":
     psr = argparse.ArgumentParser()
     psr.add_argument('-o', dest='opt')
     args = psr.parse_args()
-    energys = np.arange(1,10,0.1)
-    betas = np.sqrt(energys**2-m_0**2)/energys
-    n_betas = 1-betas**2
+    k_energys = np.arange(0.1, 10, 0.1)
+    energys = k_energys + m_0
+    betas = np.sqrt(energys**2 - m_0**2)/energys
+    n_betas = 1 - betas**2
     # Ionization
     coeff = 1/8/np.pi/epislon_0**2*e**4/m_0*J2MeV**2/100
     coeff_ion = coeff*N*Z
@@ -39,11 +41,12 @@ if __name__=="__main__":
     dE_che = coeff_che*(1-1/n**2/betas**2)*delta_beta_2
     with PdfPages(args.opt) as pdf:
         fig, ax =plt.subplots(dpi=150)
-        ax.plot(energys, dE_ion, label='Ionization')
-        ax.plot(energys, dE_bre, label='Bremsstrahlung')
-        ax.plot(energys, dE_che, label='Cherenkov')
-        ax.set_xlabel('Energy(MeV)')
+        ax.plot(k_energys, dE_ion, label='Ionization')
+        ax.plot(k_energys, dE_bre, label='Bremsstrahlung')
+        ax.plot(k_energys, dE_che, label='Cherenkov')
+        ax.set_xlabel('Kinetic Energy(MeV)')
         ax.set_ylabel('dE/dx(MeV/cm)')
+        ax.xaxis.set_major_locator(MultipleLocator(1))
         ax.legend()
         pdf.savefig(fig)
         ax.set_yscale('log')
